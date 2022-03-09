e7be79310ec433ded252c421707c9485125615f1
Nathan <nathan.sharp@ansys.com>
2022-03-09 13:12:49 -0800

    Changed package name in python (#2)
    * Changed package name in python.
    
    * Fix two locations that weren't renamed.

--- doc/source/class_documentation.rst
+++ doc/source/class_documentation.rst
@@ -10,6 +10,6 @@ automatically generate class and method documentation.
 .. autosummary::
    :toctree: _autosummary/
 
-   ansys.product.library.Complex
+   ansys.common.variableinterop.Complex
 
 Note how each method or attribute has its own page.
--- src/ansys/common/variableinterop/__init__.py
+++ src/ansys/common/variableinterop/__init__.py
@@ -1,19 +1,19 @@
 """This init file allows python to treat directories containing it as modules.
 
-Import any methods you want exposed at your library level here.
+Import any methods you want exposed at your variableinterop level here.
 
 For example, if you want to avoid this behavior:
 
 .. code::
 
-   >>> from ansys.product.library.module import add
+   >>> from ansys.common.variableinterop.module import add
 
 Then add the import within this module to enable:
 
 .. code::
 
-   >>> from ansys.product import library
-   >>> library.add(1, 2)
+   >>> from ansys.common import variableinterop
+   >>> variableinterop.add(1, 2)
 
 .. note::
    The version is obtained from the installation metadata. During development,
@@ -28,5 +28,5 @@ except ModuleNotFoundError:
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
-from ansys.product.library.module import add
-from ansys.product.library.other_module import Complex
+from ansys.common.variableinterop.module import add
+from ansys.common.variableinterop.other_module import Complex
