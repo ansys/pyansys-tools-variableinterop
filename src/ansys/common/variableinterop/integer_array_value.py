c3fef5b83a6062a3a0ae01f5b98ade167a61772b
bfairburn-phx <brandon.fairburn@ansys.com>
2022-04-05 10:39:34 -0400

    Type stubs for array values

---
+++ src/ansys/common/variableinterop/integer_array_value.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import numpy as np
+
+import ansys.common.variableinterop.ivariable_visitor as ivariable_visitor
+import ansys.common.variableinterop.variable_value as variable_value
+
+from .variable_type import VariableType
+
+class IntegerArrayValue(np.ndarray, variable_value.IVariableValue):
+
+    def accept(
+        self,
+        visitor: ivariable_visitor.IVariableValueVisitor[variable_value.T]
+    ) -> variable_value.T:
+        return visitor.visit_integer_array(self)
+
+    def variable_type(self) -> VariableType:
+        return VariableType.INTEGER_ARRAY
--- src/ansys/common/variableinterop/ivariable_visitor.py
+++ src/ansys/common/variableinterop/ivariable_visitor.py
@@ -5,6 +5,11 @@ from typing import Generic, TypeVar
 
 import ansys.common.variableinterop.integer_value as integer_value
 import ansys.common.variableinterop.real_value as real_value
+import ansys.common.variableinterop.integer_array_value as integer_array_value
+import ansys.common.variableinterop.real_array_value as real_array_value
+import ansys.common.variableinterop.boolean_array_value as boolean_array_value
+import ansys.common.variableinterop.string_array_value as string_array_value
+import ansys.common.variableinterop.file_array_value as file_array_value
 
 T = TypeVar("T")
 
@@ -42,6 +47,81 @@ class IVariableValueVisitor(ABC, Generic[T]):
         Parameters
         ----------
         value The RealValue being visited
+
+        Returns
+        -------
+        The result
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_integer_array(self, value: integer_array_value.IntegerArrayValue) -> T:
+        """
+        Will be called if accept is called on an IntegerArrayValue
+
+        Parameters
+        ----------
+        value The IntegerArrayValue being visited
+
+        Returns
+        -------
+        The result
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_real_array(self, value: real_array_value.RealArrayValue) -> T:
+        """
+        Will be called if accept is called on an RealArrayValue
+
+        Parameters
+        ----------
+        value The RealArrayValue being visited
+
+        Returns
+        -------
+        The result
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_boolean_array(self, value: boolean_array_value.BooleanArrayValue) -> T:
+        """
+        Will be called if accept is called on an BooleanArrayValue
+
+        Parameters
+        ----------
+        value The BooleanArrayValue being visited
+
+        Returns
+        -------
+        The result
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_string_array(self, value: string_array_value.StringArrayValue) -> T:
+        """
+        Will be called if accept is called on an StringArrayValue
+
+        Parameters
+        ----------
+        value The StringArrayValue being visited
+
+        Returns
+        -------
+        The result
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_file_array(self, value: file_array_value.FileArrayValue) -> T:
+        """
+        Will be called if accept is called on an FileArrayValue
+
+        Parameters
+        ----------
+        value The FileArrayValue being visited
 
         Returns
         -------
