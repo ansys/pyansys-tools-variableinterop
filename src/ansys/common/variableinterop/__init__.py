7a417c69b67be3becd87b9bb1039e2594df67c59
Mark Koninckx <mark.koninckx@ansys.com>
2022-04-05 13:08:35 -0400

    Add BooleanValue to module __init__.

--- src/ansys/common/variableinterop/__init__.py
+++ src/ansys/common/variableinterop/__init__.py
@@ -63,6 +63,7 @@ except ModuleNotFoundError:
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
+from .boolean_value import BooleanValue
 from .coercion import implicit_coerce, implicit_coerce_single
 from .common_variable_metadata import CommonVariableMetadata
 from .integer_metadata import IntegerMetadata
