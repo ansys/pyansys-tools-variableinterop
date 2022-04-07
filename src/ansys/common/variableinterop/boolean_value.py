42c14e9ff68ba0f2a0550e00a6fc6e8f19ef2dda
Mark Koninckx <mark.koninckx@ansys.com>
2022-04-07 17:25:07 -0400

    Give BooleanValue a from_api_string implementation.

--- src/ansys/common/variableinterop/boolean_value.py
+++ src/ansys/common/variableinterop/boolean_value.py
@@ -1,9 +1,12 @@
 """Definition of BooleanValue."""
 from __future__ import annotations
 
+from typing import Dict
+
 import numpy as np
 
 import ansys.common.variableinterop.ivariable_visitor as ivariable_visitor
+import ansys.common.variableinterop.real_value as real_value
 import ansys.common.variableinterop.variable_value as variable_value
 
 from .variable_type import VariableType
@@ -31,17 +34,48 @@ class BooleanValue(np.bool_, variable_value.IVariableValue):
     def to_api_string(self) -> str:
         raise NotImplementedError
 
+    __api_str_values: Dict[str, bool] = {
+        'yes': True,
+        'y': True,
+        'true': True,
+        'no': False,
+        'n': False,
+        'false': False
+    }
+    """
+    Defines acceptable normalized values for API string conversion.
+    """
+
     @staticmethod
     def from_api_string(value: str) -> BooleanValue:
         """
         Convert an API string back into a value.
+
+        The conversion is performed according to the type
+        interoperability specifications.
+
+        Values which are parseable as floating-point numbers
+        are parsed in that manner, then converted to boolean.
+
+        Values which are non-numeric are checked to see if they match
+        the following values for True: "true", "yes", or "y"; or the
+        following values for False: "false", "no", or "n". The
+        comparison is case-insensitive.
+
+        Values not otherwise interpretable result in a ValueError.
 
         Parameters
         ----------
         value
         The string to convert.
         """
-        raise NotImplementedError
+        normalized: str = str.lower(str.strip(value))
+        if (normalized in BooleanValue.__api_str_values):
+            return BooleanValue(BooleanValue.__api_str_values[normalized])
+        else:
+            # Try to parse as real, allow exception to bubble up
+            real_equiv: real_value.RealValue = real_value.RealValue.from_api_string(normalized)
+            return real_equiv.to_boolean_value()
 
     # to_formatted_string here
 
