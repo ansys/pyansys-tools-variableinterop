1f25839ed5ec849d144ec192c89ac3467a49312d
bfairburn-phx <brandon.fairburn@ansys.com>
2022-04-08 12:50:56 -0400

    Make return type hints RealArrayValue

--- src/ansys/common/variableinterop/to_real_array_visitor.py
+++ src/ansys/common/variableinterop/to_real_array_visitor.py
@@ -6,37 +6,34 @@ from ansys.common.variableinterop.integer_array_value import IntegerArrayValue
 from ansys.common.variableinterop.real_array_value import RealArrayValue
 from ansys.common.variableinterop.boolean_array_value import BooleanArrayValue
 from ansys.common.variableinterop.string_array_value import StringArrayValue
-from ansys.common.variableinterop.ivariable_visitor import IVariableValueVisitor, T
-
+from ansys.common.variableinterop.ivariable_visitor import IVariableValueVisitor
 from ansys.common.variableinterop.variable_type import VariableType
 from ansys.common.variableinterop.exceptions import IncompatibleTypesException
-
-import numpy as np
 
 
 class ToRealArrayVisitor(IVariableValueVisitor):
     """Visitor pattern to call conversion methods to RealArrayValue"""
 
-    def visit_integer(self, value: IntegerValue) -> T:
+    def visit_integer(self, value: IntegerValue) -> RealArrayValue:
         raise IncompatibleTypesException(value.variable_type, VariableType.REAL_ARRAY)
 
-    def visit_real(self, value: RealValue) -> T:
+    def visit_real(self, value: RealValue) -> RealArrayValue:
         raise IncompatibleTypesException(value.variable_type, VariableType.REAL_ARRAY)
 
-    def visit_boolean(self, value: BooleanValue) -> T:
+    def visit_boolean(self, value: BooleanValue) -> RealArrayValue:
         raise IncompatibleTypesException(value.variable_type, VariableType.REAL_ARRAY)
 
-    def visit_string(self, value: StringValue) -> T:
+    def visit_string(self, value: StringValue) -> RealArrayValue:
         raise IncompatibleTypesException(value.variable_type, VariableType.REAL_ARRAY)
 
-    def visit_integer_array(self, value: IntegerArrayValue) -> T:
+    def visit_integer_array(self, value: IntegerArrayValue) -> RealArrayValue:
         return value.to_real_array_value()
 
-    def visit_real_array(self, value: RealArrayValue) -> T:
+    def visit_real_array(self, value: RealArrayValue) -> RealArrayValue:
         return value
 
-    def visit_boolean_array(self, value: BooleanArrayValue) -> T:
+    def visit_boolean_array(self, value: BooleanArrayValue) -> RealArrayValue:
         return value.to_real_array_value()
 
-    def visit_string_array(self, value: StringArrayValue) -> T:
+    def visit_string_array(self, value: StringArrayValue) -> RealArrayValue:
         return value.to_real_array_value()
