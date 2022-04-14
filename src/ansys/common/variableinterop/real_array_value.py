256f37b65e573156f801b9e84d7ef6dc20cdaeee
bfairburn-phx <brandon.fairburn@ansys.com>
2022-04-14 10:47:37 -0400

    Add CommonArrayValue

--- src/ansys/common/variableinterop/real_array_value.py
+++ src/ansys/common/variableinterop/real_array_value.py
@@ -3,18 +3,18 @@ from __future__ import annotations
 from typing import TypeVar
 
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
+from numpy.typing import ArrayLike
 from overrides import overrides
 
 import ansys.common.variableinterop.boolean_array_value as boolean_array_value
 import ansys.common.variableinterop.ivariable_visitor as ivariable_visitor
 import ansys.common.variableinterop.variable_type as variable_type
-import ansys.common.variableinterop.variable_value as variable_value
+from ansys.common.variableinterop.variable_value import CommonArrayValue
 
 T = TypeVar("T")
 
 
-class RealArrayValue(NDArray[np.float64], variable_value.IVariableValue):
+class RealArrayValue(CommonArrayValue[np.float64]):
     """Array of real values.
 
     In Python RealArrayValue is implemented by extending NumPy's ndarray type. This means that
