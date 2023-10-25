5cf9748dcf9ae1c55b8989ac2e08d768d1e09bee
Camille <78221213+clatapie@users.noreply.github.com>
2023-10-25 16:03:02 +0200

    fix: fixing docformatter

--- src/ansys/tools/variableinterop/utils/string_escaping.py
+++ src/ansys/tools/variableinterop/utils/string_escaping.py
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """Provides functions for ModelCenter-standard string escaping."""
 from typing import Dict, List
 
