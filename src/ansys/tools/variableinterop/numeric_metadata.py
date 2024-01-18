da43e0d61640a487edd7c5ae1b28a37894d10f06
Camille <78221213+clatapie@users.noreply.github.com>
2024-01-18 11:00:07 +0100

    fix: license headers

--- src/ansys/tools/variableinterop/numeric_metadata.py
+++ src/ansys/tools/variableinterop/numeric_metadata.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2024 ANSYS, Inc. and/or its affiliates.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 """Defines the ``NumericMetadata`` class."""
 from __future__ import annotations
 
--- src/ansys/tools/variableinterop/utils/__init__.py
+++ src/ansys/tools/variableinterop/utils/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2024 ANSYS, Inc. and/or its affiliates.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -19,4 +19,5 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 """Provides utility methods and classes used by the main package."""
