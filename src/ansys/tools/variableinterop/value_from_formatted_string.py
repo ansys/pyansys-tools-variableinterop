f266a58066e4049f146a99a6c36aec4f69b0ea0c
Camille <78221213+clatapie@users.noreply.github.com>
2024-01-19 09:37:04 +0100

    fix: removing line after license

--- src/ansys/tools/variableinterop/value_from_formatted_string.py
+++ src/ansys/tools/variableinterop/value_from_formatted_string.py
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """Provides a method for converting a formatted string to a value."""
 from numpy import unicode_
 
--- tests/test_implicit_coercion.py
+++ tests/test_implicit_coercion.py
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """Tests for the implicit type coercion module."""
 from abc import ABC, abstractmethod
 from typing import Any, Optional, Tuple, Type
